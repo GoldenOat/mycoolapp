aa3a59f3e29e939b9a948a79c7da7751898acb4c
alexis <alexistang0822@gmail.com>
2022-09-27 12:53:34 -0400

    springBootApp

---
+++ src/main/java/com/alexis/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,17 @@
+package com.alexis.springboot.demo.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+public class FunRestController {
+    @GetMapping("/")
+    public String sayHello() {
+        return "hello world Time on server is " + LocalDateTime.now();
+    }
+
+
+
+}
